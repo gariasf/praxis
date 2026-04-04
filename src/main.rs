dff35bc4122c005d7c29ad529bf066af09351ea7
Guillem Arias Fauste <hello@gariasf.com>
2026-04-04 13:20:20 +0200

    open window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowId},
+};
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::Resized(_new_size) => {
+                // Later, when we have a renderer, we'll need to resize it here.
+                // Something like:
+                // surface_config.width = new_size.width;
+                // surface_config.height = new_size.height;
+                // surface.configure(&device, &surface_config);
+            }
+            WindowEvent::RedrawRequested => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in AboutToWait, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+
+                // Draw.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw in
+                // applications which do not always need to. Applications that redraw continuously
+                // can render here instead.
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => {}
+        }
+    }
+}
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
 fn main() {
-    println!("Hello, Praxis!");
+    tracing_subscriber::fmt::init();
+
+    let event_loop = EventLoop::new().unwrap();
+
+    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+    // dispatched any events. This is ideal for games and similar applications.
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    let _ = event_loop.run_app(&mut app);
 }
